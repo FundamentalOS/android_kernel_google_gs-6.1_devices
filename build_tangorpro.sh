de455f4b9fbae73793a88fd905a8d3918f44b376
Robin Peng <robinpeng@google.com>
2024-01-08 10:34:57 +0000

    Migrate common to aosp
    Bug: 316155868
    Change-Id: Ie607c38fbc430a9bfe7232301e7ae8483985729e
    Signed-off-by: Robin Peng <robinpeng@google.com>

--- build_tangorpro.sh
+++ build_tangorpro.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0
 
-exec tools/bazel run --config=tangorpro --config=fast //private/devices/google/tangorpro:tangorpro_dist "$@"
+exec tools/bazel run --config=tangorpro --config=fast --config=stamp //private/devices/google/tangorpro:tangorpro_dist "$@"
 
